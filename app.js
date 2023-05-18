b84a9dfea26d21183ae2536fc5f9aa03f433b227
dawood <dawood.altai111@gmail.com>
2023-05-18 22:33:16 +0300

    calculator app

---
+++ app.js
@@ -0,0 +1,10 @@
+const zero = document.querySelector("#zero")
+const display = document.querySelector(".display")
+
+display.innerHTML=`hi`;
+zero.addEventListener("onclick" ,function() {
+value
+    const btnZero = zero.value;
+    display.innerHTML=btnZero.value;
+
+} )
